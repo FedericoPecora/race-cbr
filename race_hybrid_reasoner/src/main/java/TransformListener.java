cee22f44e6652301957b5b200890a7f0bdb7e4e5
iran.mansoori85 <iran.mansoori85@72d639c1-6fe6-f3f0-948b-9f2a993f1a26>
2013-12-20 14:17:39 +0000

    rosjava tf plus halfly hard coded stuffs works

---
+++ race_hybrid_reasoner/src/main/java/TransformListener.java
@@ -0,0 +1,51 @@
+/*
+ * copied from: https://code.google.com/p/rosjava/source/browse/android_honeycomb_mr2/src/org/ros/android/views/visualization/?repo=android&r=07c00460a3826b976c153ea57353a54a4b275e37
+ */
+
+import org.ros.message.MessageListener;
+import geometry_msgs.TransformStamped;
+import tf.tfMessage;
+
+import org.ros.namespace.GraphName;
+import org.ros.node.AbstractNodeMain;
+import org.ros.node.ConnectedNode;
+import org.ros.node.Node;
+import org.ros.node.topic.Subscriber;
+
+public class TransformListener extends AbstractNodeMain {
+
+  private Transformer transformer = new Transformer();
+  private Subscriber<tfMessage> tfSubscriber;
+
+  public Transformer getTransformer() {
+    return transformer;
+  }
+
+  public void setTransformer(Transformer transformer) {
+    this.transformer = transformer;
+  }
+
+  @Override
+  public void onStart(final ConnectedNode node) {
+    transformer.setPrefix(GraphName.of(node.getParameterTree().getString("~tf_prefix", "")));
+    tfSubscriber = node.newSubscriber(GraphName.of("tf"), tf.tfMessage._TYPE); 
+    tfSubscriber.addMessageListener(new MessageListener<tfMessage>() {
+      @Override
+      public void onNewMessage(tfMessage message) {
+        for (TransformStamped transform : message.getTransforms()) {
+          transformer.updateTransform(transform);
+        }
+      }
+    });
+  }
+
+  @Override
+  public void onShutdown(Node node) {
+    tfSubscriber.shutdown();
+  }
+
+  @Override
+  public GraphName getDefaultNodeName() {
+	return GraphName.of("rosjava_tf_example");
+  }
+}
